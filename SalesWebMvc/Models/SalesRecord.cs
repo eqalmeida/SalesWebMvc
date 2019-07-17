6837ba32dfa73eb8ebf1212b20d0e3dc81094f5f
Eduardo Almeida <eqalmeida@gmail.com>
2019-07-16 21:37:26 -0300

    Initial commit

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amont { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+        public SalesRecord(int id, DateTime date, double amont, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amont = amont;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
